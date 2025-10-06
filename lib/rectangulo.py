23058c242068d68ba8870f83c0e482f24ac54168
Iria Campos Seoane <iria.campos.seoane@rai.usc.es>
2025-10-06 15:54:50 +0200

    Agregadas funcionalidades para el rectángulo

---
+++ lib/rectangulo.py
@@ -0,0 +1,9 @@
+def get_area(lado1:int, lado2:int) -> int:
+    return lado1*lado2
+
+def get_identificador() -> str:
+    return "rectangulo"
+
+def get_perimetro(lado1:int, lado2:int) -> int:
+    return lado1*2 + lado2*2
+
